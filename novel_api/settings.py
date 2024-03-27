852d6a359dafa899049e7908be6344829771165c
XuanThongN <thong.xuan.vft@gmail.com>
2024-03-27 11:35:26 +0700

    feat: add auth

--- novel_api/settings.py
+++ novel_api/settings.py
@@ -98,7 +98,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.pagination.PageNumberPagination',
+    ],
     'PAGE_SIZE': 10
 }
 
