959031f050d21ea9830ca7967bf49e37e81c971b
XuanThongN <thong.xuan.vft@gmail.com>
2024-03-27 11:33:21 +0700

    feat: create model serializers

--- novel_api/backend/serializers.py
+++ novel_api/backend/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from .models import Category, Novel, Chapter, Comment
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,34 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['url', 'id', 'name']
+
+
+class NovelSerializer(serializers.HyperlinkedModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+
+    class Meta:
+        model = Novel
+        fields = ['url', 'id', 'title', 'author', 'category']
+
+
+class ChapterSerializer(serializers.HyperlinkedModelSerializer):
+    novel = serializers.PrimaryKeyRelatedField(queryset=Novel.objects.all())
+
+    class Meta:
+        model = Chapter
+        fields = ['url', 'id', 'title', 'content', 'novel']
+
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    novel = serializers.PrimaryKeyRelatedField(queryset=Novel.objects.all())
+
+    class Meta:
+        model = Comment
+        fields = ['url', 'id', 'text', 'user', 'novel']
