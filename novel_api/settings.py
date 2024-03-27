fb6647a169bc90bc9eaeabb9f7a5285b7e92843f
XuanThongN <thong.xuan.vft@gmail.com>
2024-03-27 15:29:15 +0700

    fix: auth setting

--- novel_api/settings.py
+++ novel_api/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'novel_api'
+    'novel_api',
 ]
 
 MIDDLEWARE = [
@@ -98,10 +98,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.pagination.PageNumberPagination',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
