10e5aee332ff8d8ae0af36da193debfae712c7ba
XuanThongN <thong.xuan.vft@gmail.com>
2024-03-27 11:33:02 +0700

    feat: create models

--- novel_api/backend/models.py
+++ novel_api/backend/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Novel(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='novels')
+
+    def __str__(self):
+        return self.title
+
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    novel = models.ForeignKey(Novel, on_delete=models.CASCADE, related_name='chapters')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    novel = models.ForeignKey(Novel, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.text
