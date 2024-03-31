1618be76d154d6c0363ec810865721ce84c621b5
XuanThongN <thong.xuan.vft@gmail.com>
2024-03-31 21:25:33 +0700

    fix: time zone is incorrect when save data to db

--- novel_api/settings.py
+++ novel_api/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 
@@ -188,14 +187,13 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 # STATICFILES_STORAGE =
 MEDIA_URL = '/media/'
